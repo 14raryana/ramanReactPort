cbf389ba02dca8c5aedf73717f80d4037f2cb226
Raman Aryana <14raryana@gmail.com>
2021-02-13 17:23:46 -0500

    React Portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+## React Portfolio
+
+https://dthorpe13.github.io/React_Portfolio/
---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Raman Aryana",
+    "role": "M.E.R.N. Stack Developer",
+    "linkedinId":" 14raryana",
+    "email": "14raryana@gmail.com",
+    "number": "216-978-0564",
+    "github": "14raryana",
+    "roleDescription": " utilizing my growing expertise in creating fully responsive, interactive web applications. After recently graduating from the Case Western Reserve Full Stack Development Bootcamp, I am focusing on a career shift from Construction into Web Development.",
+    // "socialLinks":[
+    //     {
+    //       "name":"linkedin",
+    //       "url":"https://www.linkedin.com/in/dthorpe13/",
+    //       "className":"fa fa-linkedin"
+    //     },
+    //     {
+    //       "name":"github",
+    //       "url":"https://github.com/DThorpe13",
+    //       "className":"fa fa-github"
+    //     },
+    //     {
+    //       "name":"twitter",
+    //       "url":"https://twitter.com/d_thorpe13",
+    //       "className":"fa fa-twitter"
+    //     },
+    //     {
+    //       "name":"facebook",
+    //       "url":"https://www.facebook.com/profile.php?id=100008767456435",
+    //       "className":"fa fa-facebook"
+    //     },
+    //     {
+    //       "name":"instagram",
+    //       "url":"https://www.instagram.com/d_thorpe13/?hl=en",
+    //       "className":"fa fa-instagram"
+    //     },
+    //     {
+    //       "name":"file",
+    //       "url":"https://docs.google.com/document/d/1dCvdz3ODj6Osz0rN_EBXH6t7ckF3eVqgDwqQxhDlgYA/edit#heading=h.f655783yxz0y",
+    //       "className":"fa fa-file"
+    //     }
+    //   ],
+    "aboutme":"",
+    "address":"Cleveland, Ohio",
+    "website":"14raryana@gmail.com",
+    "education":[
+      {
+        "UniversityName":"CWRU Bootcamp, John Carroll University",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Feb",
+        "YearOfPassing":"2021",
+        "Achievements":"HTML | CSS | Bootstrap | JavaScript | JQuery | React | Express | MySQL | MongoDB"
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Lawhon Construction",
+        "specialization":"Project Manager",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Current",
+        "Achievements":"Organizing, overseeing and working through residential contruction projects."
+      },
+      
+    ],
+    "skillsDescription":"Skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Quaran-Tini",
+        "description":"",
+        "url": "https://quaran-tini.herokuapp.com/",
+        "imgurl":"images/Eon Playlist.png"
+      },
+      {
+        "name":"Weather Dashboard",
+        "description":"", 
+        "url": "https://14raryana.github.io/openWeatherDashboard/", 
+        "imgurl":"images/WeatherDash.png"
+      },
+      {
+        "name":"Fitness Tracker",
+        "description":"",
+        "url": "https://calm-earth-53961.herokuapp.com/?id=5ff30f8104b933001756361a",
+        "imgurl":"images/Fitness Tracker.png"
+      },
+      {
+        "name":"Burger Bites",
+        "description":"",
+        "url": "https://burger-create.herokuapp.com/",
+        "imgurl":"images/Burger App.png"
+      }
+    ]
+  }
+  
+  export default resumeData
